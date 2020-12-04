0a5f000f171d036d15f36bb50a552f3ea422e09f
tom <treilly94@live.com>
2020-12-04 13:44:35 +0000

    added custom domain

--- terraform/modules/azure/main.tf
+++ terraform/modules/azure/main.tf
@@ -1,7 +1,20 @@
+locals {
+  org_name = "tiredanimal"
+  cloud_name = "azure"
+
+  custom_domain = "${local.org_name}.com"
+  custom_path = "${var.name}.${var.location}.${local.cloud_name}"
+  custom_full_url = "${local.custom_path}.${local.custom_domain}"
+
+  storage_account_name = "${local.org_name}${var.name}"
+  azure_domain = "z33.web.core.windows.net"
+  azure_full_url = "${local.storage_account_name}.${local.azure_domain}"
+}
+
 resource "cloudflare_record" "this" {
   zone_id = "1804341c8b633f83d43dab12a5dfcfb1"
-  name    = "staticsite.azure"
-  value   = regex("https://(.*)/", azurerm_storage_account.this.primary_web_endpoint)[0]
+  name    = local.custom_path
+  value   = local.azure_full_url
   type    = "CNAME"
   ttl     = 3600
 }
@@ -12,7 +25,7 @@ resource "azurerm_resource_group" "this" {
 }
 
 resource "azurerm_storage_account" "this" {
-  name                     = var.name
+  name                     = local.storage_account_name
   resource_group_name      = azurerm_resource_group.this.name
   location                 = var.location
   account_tier             = "Standard"
@@ -21,6 +34,12 @@ resource "azurerm_storage_account" "this" {
   static_website {
       index_document = "index.html"
   }
+
+  custom_domain {
+    name = local.custom_full_url
+  }
+
+  depends_on = [cloudflare_record.this]
 }
 
 resource "azurerm_storage_blob" "this" {
--- terraform/modules/azure/variables.tf
+++ terraform/modules/azure/variables.tf
@@ -1,5 +1,5 @@
 variable "name" {
-    default = "tiredanimalstaticwebsite"
+    default = "staticwebsite"
 }
 
 variable "location" {
