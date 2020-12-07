c031527b533a168e2e6768ecd750642c91c879c8
tom <treilly94@live.com>
2020-12-07 21:34:04 +0000

    added aws provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,15 @@
+provider "cloudflare" {
+  version = "~> 2.0"
+}
+
 provider "azurerm" {
   version = "2.38.0"
   features {}
 }
 
-provider "cloudflare" {
-  version = "~> 2.0"
+provider "aws" {
+  version = "3.2.0"
+  region = "eu-west-2"
 }
 
 module "azure" {
