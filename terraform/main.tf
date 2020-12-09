e65dd44c2a668e84ccb71d8aa644a8b6a0deefe8
tom <treilly94@live.com>
2020-12-09 20:11:59 +0000

    moved providers to seperate file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,3 @@
-provider "cloudflare" {
-  version = "~> 2.0"
-}
-
-provider "azurerm" {
-  version = "2.38.0"
-  features {}
-}
-
-provider "aws" {
-  version = "3.2.0"
-  region = "eu-west-2"
-}
-
 module "azure" {
   source = "./modules/azure"
 
